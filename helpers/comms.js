dda237b797d2320d878b0afff323cdb0a133cf61
Nick <nick.alex.ritchie@gmail.com>
2021-08-21 13:34:08 +1000

    re designing prompt sequences to allow for specific and exit prompts

--- helpers/comms.js
+++ helpers/comms.js
@@ -1,34 +1,45 @@
 const mysql = require('mysql2/promise');
 
+
+async function connection(){
+    let open_conn = await mysql.createConnection({
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    });
+    return open_conn;
+}
+    
+
 // department commands
 /**
  * A function to return all departments
- * @param {mysql.connection} connection 
  * @param {name} string Name of department * not this is a uniquely constrained column 
  */
-const getAllDepartments = async (connection) => {
+async function getAllDepartments() {
     let query = 'SELECT * FROM department';
     console.log('Getting all departments from database');
-
+    let rows = [];
     try{
-        const[rows, _] = await connection.execute(query);
-        return rows;
+        let conn = await connection();
+        [rows, _] = await conn.execute(query);
     } catch(error){
         console.error(error);
     }
-
+    return rows;
 }
 
 /**
  * A function to add a new department with a given name
- * @param {mysql.connection} connection 
  * @param {name} string Name of department * not this is a uniquely constrained column 
  */
-const addDepartmentByName = async(connection, name) => {
+const addDepartmentByName = async(name) => {
     try{
         let query = 'INSERT INTO department(name) VALUES(?)';
         console.log(`Adding department ${name} to db`);
-        const[rows, _] = await connection.execute(query, [name]);
+        let conn = await connection();
+        const[rows, _] = await conn.execute(query, [name]);
         return rows;
     } catch(err){
         console.error(err);
@@ -36,16 +47,15 @@ const addDepartmentByName = async(connection, name) => {
 }
 
 /**
- * 
- * @param {mysql.connection} connection 
  * @param {id} int the id of the target row to update
  * @param {newName} string the new name to update to
  */
-const updateDepartmentNameById = async(connection, id, newName) => {
+const updateDepartmentNameById = async(id, newName) => {
     try{
         let query = "UPDATE department SET name = ? WHERE id = ?";
         console.log(`Updating department at id:${id} to ${newName}`)
-        const[rows, _] = await connection.execute(query, [newName, id]);
+        let conn = await connection();
+        const[rows, _] = await conn.execute(query, [newName, id]);
         return rows;
     } catch(err){
         console.error(err);
@@ -55,32 +65,46 @@ const updateDepartmentNameById = async(connection, id, newName) => {
 
 /**
  * 
- * @param {mysql.connection} connection 
  * @param {id} int the id of the target row to update
  * @param {newName} string the new name to update to
  */
-const deleteDepartmentById = async(connection, id) => {
+const deleteDepartmentById = async(id) => {
     try{
         let query = "DELETE FROM department WHERE id =?";
         console.log(`Deleting department at id:${id}`)
-        const[rows, _] = await connection.execute(query, [id]);
+        let conn = await connection();
+        const[rows, _] = await conn.execute(query, [id]);
         return rows;
     } catch(err){
         console.error(err);
     }
 }
-
-
 
 // role commands
 
 // employee commands
+
+
+// general database commands
+const getTables = async() => {
+    try{
+        let query = "SHOW TABLES";
+        let conn = await connection();
+        const[rows, _] = await conn.execute(query);
+        return rows;
+    } catch(err){
+        console.error(err);
+    }
+}
 
 const comms = {
     getAllDepartments,
     addDepartmentByName,
     updateDepartmentNameById,
     deleteDepartmentById,
+
+    // general queries
+    getTables,
 }
 
 module.exports = comms
--- index.js
+++ index.js
@@ -2,29 +2,199 @@ const comms = require('./helpers/comms');
 const inquirer = require('inquirer');
 const mysql = require('mysql2/promise');
 const cTable = require('console.table');
+const {confirmStringValidator, confirmIntValidator} = require('./helpers/validators');
 require('dotenv').config();
 
-
-
-async function main(){
-    const connection = await mysql.createConnection({
-        host: 'localhost',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME
-    });
-    let resultTable;
-
-    resultTable = await comms.addDepartmentByName(connection, "first department");
-    console.table(resultTable);
-    resultTable = await comms.addDepartmentByName(connection, "second department");
-    console.table(resultTable);
-    resultTable = await comms.updateDepartmentNameById(connection, 1, "updated-name");
-    console.table(resultTable);
-    resultTable = await comms.deleteDepartmentById(connection, 2);
-    console.table(resultTable);
-    resultTable = await comms.getAllDepartments(connection);
-    console.table(resultTable);
+async function specificPrompt(){
+    return inquirer.prompt([{
+        type: 'rawlist',
+        message: 'What would you like to do',
+        name: 'rootAction',
+        choices: [
+            'view',
+            'update',
+            'add',
+            'delete',
+        ]
+    }]).then(async (answer) => {
+        switch (answer.rootAction){
+            case 'view': {
+                viewPrompt();
+                break
+            }
+            case 'update': {
+                updatePrompt();
+                break
+            }
+            case 'add': {
+                addPrompt();
+                break
+            }
+            case 'delete': {
+                deletePrompt();
+                break
+            }
+        }
+    })
 }
 
-main();
+/**
+ * Determines the table the user wants to view, then calls viewTable Prompt
+ * @param {mysql.connection} connection 
+ * @returns None
+ */
+async function viewPrompt(){
+    let tables = comms.getTables();
+    return inquirer.prompt([{
+        type: 'list',
+        message: 'Which table would you like to view',
+        name: 'table',
+        choices: tables
+    }]).then(async (answer) => {
+        viewTablePrompt(connection, answer.table);
+    })
+}
+
+/**
+ * Determines the view mode the user wants, then calls viewTableHow Prompt
+ * @param {mysql.connection} connection 
+ * @returns None
+ */
+async function viewTablePrompt(tableName){
+    let tables = comms.getTables();
+    return inquirer.prompt([{
+        type: 'list',
+        message: `How would you like to view ${tableName}`,
+        name: 'how',
+        choices: tables
+    }]).then(async (answer) => {
+        viewTableHow(answer.how);
+    })
+}
+
+async function updatePrompt(){
+    console.log('View promp sequence');
+    
+}
+async function addPrompt(){
+    console.log('View promp sequence');
+    
+}
+async function deletePrompt(){
+    console.log('View promp sequence');
+    
+}
+
+
+async function addDepartmentPrompt(){
+    // add a department
+    await inquirer.prompt({
+        type: 'input',
+        message: 'Name of department?',
+        validate: confirmStringValidator,
+        name: 'departmentName',
+    }).then(async (answer) => {
+        await comms.addDepartmentByName(connection, answer.departmentName);
+        resultTable = await comms.getAllDepartments();
+        console.table(resultTable);
+    }).catch((err) => {
+        console.error(err);
+    })
+}
+
+async function updateEmployeePrompt(){
+    // query for office number then append answer to baseAnswers
+    await inquirer.prompt({
+        type: 'rawlist',
+        message: 'What do you want to update?',
+        choices:['first name', 'last name', 'role','manager'],
+        name: 'updateType',
+    })
+    .then(async (answer) => {
+        switch (answer.updateType){
+            case 'first name': {
+                console.log('updating first name');
+                break
+            }
+            case 'last name': {
+                console.log('updating last name');
+                break
+            }
+            case 'role': {
+                console.log('updating role');
+                break
+            }
+            case 'manager': {
+                console.log('updating manager');
+                break
+            }  
+        }
+    })
+    .catch((err) =>{
+        console.error(err);
+    })
+}
+
+
+async function rootPromptUser(){
+    inquirer.prompt([{
+        type: 'rawlist',
+        message: 'What would you like to do',
+        name: 'rootAction',
+        choices: [
+            'view all departments',
+            'view all roles',
+            'view all employees',
+            'add a department',
+            'add a role',
+            'add an employee',
+            'update an employee',
+            new inquirer.Separator(),
+            'more specific commands',
+            new inquirer.Separator(),
+            'exit employee cms shell'
+        ]
+    }]).then(async (answer) => {
+        console.log(answer);
+        switch (answer.rootAction){
+            case 'view all departments':{
+                resultTable = await comms.getAllDepartments();
+                console.table(resultTable);
+                break
+            }
+            case 'view all roles': {
+                break
+            }
+            case 'view all employees': {
+                break
+            }
+            case 'add a department': {
+                await addDepartmentPrompt();
+                break
+            }
+            case 'add a role': {
+                break
+            }
+            case 'add an employee': {
+                break
+            }
+            case 'update an employee': {
+                await updateEmployeePrompt();
+                break
+            }
+            case 'more specific commands': {
+                await specificPrompt();
+                break
+            }
+            case 'exit employee cms shell':{
+                return
+            }
+        }
+    }).then(()=>{
+        rootPromptUser()
+    }).catch((err) => {
+        console.error(err);
+    })
+}
+
+rootPromptUser();
