af6decf179472542bf2e5a5a85cf2a46bddf086a
Nick <nick.alex.ritchie@gmail.com>
2021-08-23 16:01:01 +1000

    fixed tests for validators, removed unfinished tests

--- __tests__/connection.test.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../db/connection');
-
-describe('Connection tests', () => {
-    // HP
-    it('Should establish connection', () => {
-        let result = false
-        expect(result).toBe(true);
-    });
-});
--- __tests__/validators.test.js
+++ __tests__/validators.test.js
@@ -1,34 +1,34 @@
-const {confirmStringValidator, confirmIntValidator} = require('../helpers/validators');
+const {validators} = require('../helpers/validators');
 
 describe('String validator testing', ()=>{
     // unhappy path
     it('Should return bool false when passed integer as bad name', async () => {
-        const result = await confirmStringValidator(0);
+        const result = await validators.confirmStringValidator(0);
         expect(result).toBe(false);
     });
     // unhappy path
     it('Should return bool false when passed empty string as name', async () => {
-        const result = await confirmStringValidator('');
+        const result = await validators.confirmStringValidator('');
         expect(result).toBe(false);
     });
     // unhappy path - should reject parsable numbers
     it('Should return bool false when passed string that can be cast to int', async () => {
-        const result = await confirmStringValidator('12345');
+        const result = await validators.confirmStringValidator('12345');
         expect(result).toBe(false);
     });
     // UHP should reject strings containing special characters
     it('Should return false when pass a sql string containing special characters', async() => {
-        const result = await confirmStringValidator(') DROP TABLE department;');
+        const result = await validators.confirmStringValidator(') DROP TABLE department;');
         expect(result).toBe(false);
     })
     // HP should accept strings containing hyphen special character only
     it('Should return false when pass a sql string containing special characters', async() => {
-        const result = await confirmStringValidator('Sales-Force');
+        const result = await validators.confirmStringValidator('Sales-Force');
         expect(result).toBe(true);
     })
     // happy path
     it('Should return true boolean when passed valid string', async () => {
-        const result = await confirmStringValidator('Valid string');
+        const result = await validators.confirmStringValidator('Valid string');
         expect(result).toBe(true);
     });
 });
@@ -36,17 +36,17 @@ describe('String validator testing', ()=>{
 describe('Id validator testing', ()=>{
     // unhappy path
     it('Should return bool false when passed string as bad id', async () => {
-        const result = await confirmIntValidator('Not an Integer');
+        const result = await validators.confirmIntValidator('Not an Integer');
         expect(result).toBe(false);
     });
     // unhappy path
     it('Should return bool false when passed string with mixed alpha numerics', async () => {
-        const result = await confirmIntValidator('3f4');
+        const result = await validators.confirmIntValidator('3f4');
         expect(result).toBe(false);
     });
     // happy path
     it('Should return true boolean when passed valid int', async () => {
-        const result = await confirmIntValidator(1);
+        const result = await validators.confirmIntValidator(1);
         expect(result).toBe(true);
     });
 });
